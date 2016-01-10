ea7c7a7b715f256cb08f3f31e14deec1c099dbc3
hasan-mh-aziz <hasan.mh.aziz@gmail.com>
2016-01-11 05:44:57 +0600

    minor change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/twitterTest/';
+$config['base_url'] = 'https://muktosoft-hasan.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
